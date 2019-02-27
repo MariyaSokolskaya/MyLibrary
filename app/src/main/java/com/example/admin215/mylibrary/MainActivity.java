68347825410497269f1efdc7d0498caf71bd24e0
MariyaSokolskaya <faenna@inbox.ru>
2019-02-27 17:33:08 +0700

    SimpleAdapterExample Insert Interface Query Interface

--- app/src/main/java/com/example/admin215/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/admin215/mylibrary/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.admin215.mylibrary;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -62,29 +63,55 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        showAllButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sdb = dbHelper.getReadableDatabase();
+                Cursor response = sdb.query(OpenHelper.TABLENAME, new String[]
+                        {OpenHelper.AUTHOR_COLUMN, OpenHelper.TITLE_COLUMN, OpenHelper.GENRE_COLUMN,
+                        OpenHelper.YEAR_COLUMN}, null, null, null, null, null);
+                String strQuery = "SELECT " + OpenHelper.TITLE_COLUMN + " FROM " +
+                        OpenHelper.TABLENAME + " WHERE " + OpenHelper.YEAR_COLUMN + ">2000;";
+                Cursor resp = sdb.rawQuery(strQuery, null);
+                response.moveToFirst();
+                while (response.moveToNext()){
+                    String aStr="", tStr="", gStr="";
+                    int year;
+                    aStr = response.getString(response.getColumnIndex(OpenHelper.AUTHOR_COLUMN));
+                    tStr = response.getString(response.getColumnIndex(OpenHelper.TITLE_COLUMN));
+                    gStr = response.getString(response.getColumnIndex(OpenHelper.GENRE_COLUMN));
+                    year = response.getInt(response.getColumnIndex(OpenHelper.YEAR_COLUMN));
+                    Book newBook = new Book(aStr, tStr, gStr, year);
+                    listBooks.add(newBook);
+                }
+                response.close();
+                ArrayList<HashMap<String, Object>> dataList = new ArrayList<>();
+                for (int i = 0; i < listBooks.size(); i++) {
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("author", listBooks.get(i).author);
+                    map.put("name", listBooks.get(i).name);
+                    map.put("year", listBooks.get(i).year);
+                    map.put("genre", listBooks.get(i).genre);
+                    // map.put("cover", listBooks.get(i).coverID);
+                    dataList.add(map);
+                }
+                String [] from = {"author", "name", "year", "genre"/*, "cover"*/};
+                int [] to = {R.id.author, R.id.name, R.id.year, R.id.genre/*, R.id.cover*/};
+                SimpleAdapter adapter = new SimpleAdapter(getBaseContext(), dataList, R.layout.item_list,from, to);
+                listView = (ListView) findViewById(R.id.listview);
+                listView.setAdapter(adapter);
+                listBooks.clear();
+            }
+        });
 
-        listBooks.add(new Book("Oruell", "1984", "Antiutopiya", 1992, R.drawable.oruell));
+       /* listBooks.add(new Book("Oruell", "1984", "Antiutopiya", 1992, R.drawable.oruell));
         listBooks.add(new Book("Freid", "История фобий пятилетнего мальчика", "Психология, научная " +
                                 "литература", 1955, R.drawable.fraid));
         listBooks.add(new Book("Azimov", "Основание", "Научная фантастика", 1986, R.drawable.azimov));
         listBooks.add(new Book("Rouling", "Гарри Поттер", "Фентези", 2010, R.drawable.harry));
+        */
 
-        ArrayList<HashMap<String, Object>> dataList = new ArrayList<>();
-        for (int i = 0; i < listBooks.size(); i++) {
-            HashMap<String, Object> map = new HashMap<>();
-            map.put("author", listBooks.get(i).author);
-            map.put("name", listBooks.get(i).name);
-            map.put("year", listBooks.get(i).year);
-            map.put("genre", listBooks.get(i).genre);
-            map.put("cover", listBooks.get(i).coverID);
-            dataList.add(map);
-        }
-        String [] from = {"author", "name", "year", "genre", "cover"};
-        int [] to = {R.id.author, R.id.name, R.id.year, R.id.genre, R.id.cover};
-        SimpleAdapter adapter = new SimpleAdapter(this, dataList, R.layout.item_list,from, to);
-        listView = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(adapter);
-/*        adapter = new ArrayAdapter(this, R.layout.item_list, R.id.itemlist, listBooks);
+/*      adapter = new ArrayAdapter(this, R.layout.item_list, R.id.itemlist, listBooks);
         listView = (ListView) findViewById(R.id.listview);
 
         listView.setAdapter(adapter);
